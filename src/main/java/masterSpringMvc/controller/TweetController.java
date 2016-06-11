6a0acc03b05defa3e9f43d58debcd1a93ad4718b
Christian Lehr <christian_lehr.1@gmx.de>
2016-06-11 21:49:41 +0200

    added twitter API

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 /**
@@ -42,6 +41,7 @@ public class TweetController {
     @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
     public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String search = request.getParameter("search");
+
         redirectAttributes.addAttribute("search", search);
         return "redirect:result";
     }
