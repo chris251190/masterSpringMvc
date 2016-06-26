807001ce0a96e7ab535fb8d501e04df015208f86
Christian Lehr <christian_lehr.1@gmx.de>
2016-06-26 23:19:48 +0200

    changed application.properties

--- src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebSecurityConfiguration.java
@@ -10,18 +10,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requiresChannel()
-                .anyRequest()
-                .requiresSecure()
-                .and()
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/profile")
                 .and()
-                .logout().logoutSuccessUrl("/login")
+                .logout()
+                .logoutSuccessUrl("/login")
                 .and()
                 .authorizeRequests()
                 .antMatchers("/webjars/**", "/login", "/signin/**", "/signup").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest()
+                .authenticated();
     }
 }
